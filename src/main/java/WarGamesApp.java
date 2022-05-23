cfc10c42a0a5e7379d5cb7a61fe19047f87fb328
eirilovi <eirilovi@stud.ntnu.no>
2022-05-23 16:16:29 +0200

    app class

---
+++ src/main/java/WarGamesApp.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.logging.Logger;
+
+public class WarGamesApp extends Application {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("wargames.fxml"));
+            Parent root = fxmlLoader.load();
+            Controller mainWindowController = fxmlLoader.getController();
+            Scene scene = new Scene(root, 700, 400);
+            primaryStage.setTitle("WarGames");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            logger.severe("ERROR:"  + e.getMessage());
+        }
+    }
+}
